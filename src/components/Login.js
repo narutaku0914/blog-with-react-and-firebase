73e7b5fc4e294cedce45360faa184494d96cfe03
narutaku0914 <tkm.nrsw@gmail.com>
2022-09-01 21:02:40 +0900

    login機能

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,22 @@
-const Login = () => {
-  return <div>Login</div>;
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth, provider } from "../firebase";
+
+const Login = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+  const loginWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      navigate("/");
+    });
+  };
+
+  return (
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginWithGoogle}>ホームGoogleでログイン</button>
+    </div>
+  );
 };
 export default Login;
